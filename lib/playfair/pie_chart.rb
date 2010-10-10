5cc283b87787593c6af63297d3401f2548edfa9a
Chris Dinn <chrisgdinn@gmail.com>
2010-10-10 19:41:37 -0400

    Add extremely basic render function.

--- lib/playfair/pie_chart.rb
+++ lib/playfair/pie_chart.rb
@@ -18,5 +18,11 @@ module Playfair
     def data
       @data.dup
     end
+    
+    def render(options={:width => 280, :height => 200})
+      values = @data.collect{|d| d.value }
+      labels = @data.collect{|d| d.label }
+      "http://chart.apis.google.com/chart?cht=p&chd=t:#{values.join(',')}&chl=#{labels.join('|')}&chs=280x200"
+    end
   end
 end
--- test/pie_chart_test.rb
+++ test/pie_chart_test.rb
@@ -15,4 +15,17 @@ class PieChartTest < Test::Unit::TestCase
     assert_equal "IE", pie.data.last.label
   end
   
+  def test_render
+    pie = Playfair::PieChart.new
+    
+    pie.add_data do |data|
+      data.add 90, "Firefox"
+      data.add 55, "IE"
+    end
+    
+    expected_chart = "http://chart.apis.google.com/chart?cht=p&chd=t:90,55&chl=Firefox|IE&chs=280x200"
+    
+    assert_equal expected_chart, pie.render
+  end
+  
 end
